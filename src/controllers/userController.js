bc4567e9da9b8f0b55f2be773ad04ce41b5d07f4
fabiolai <fabykalifa@gmail.com>
2022-08-02 06:39:06 -0230

    usercontroller getall signup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,27 @@ import userService from "../services/userService";
 
 class userController{
 
+    // signupcontroller
+
+    static async signUpUser (req, res) {
+        const newUser = await userService.signUpUser(req);
+        if (!newUser) {
+          return res.status(401).json({ message: "failed to register" });
+        }
+        return res.status(200).json({ message: "success", data: newUser });
+      }
+
+      //getuser
+
+      static async getUsers  (req, res) {
+        const newUser = await userService.getUsers(req);
+        if (!newUser) {
+          return res.status(401).json({ message: "failed to register" });
+        }
+        return res.status(200).json({ message: "success", data: newUser });
+      }
+    
+
 }
 
 export default userController;
